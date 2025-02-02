d7b4e8ccea9d20e7c8138894e2723b23889f3b92
Onur Efe <onur.efe44@gmail.com>
2025-02-03 02:26:04 +0300

    Update __init__.py

--- symmetry_lens/__init__.py
+++ symmetry_lens/__init__.py
@@ -1,8 +1,7 @@
 from .conditional_probability_estimator import ConditionalProbabilityEstimator
 from .probability_estimator import ProbabilityEstimator
 from .group_convolution_layer import GroupConvolutionLayer
-from .group_convolution_layer2d import GroupConvolutionLayer2d
-from .models import EquivariantAdapter, EquivariantAdapter2d
+from .models import EquivariantAdapter
 from .synthetic_data_generator import SyntheticDataGenerator
 from .train_utils import train, make_data_generator, create_model
 from .regularizations import convert_to_regularization_format
